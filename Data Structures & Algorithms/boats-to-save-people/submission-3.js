fc1ba6aca4238e5ae3424811aafd761ec96eeeb1
lucacasty <119264645+lucacasty@users.noreply.github.com>
2026-04-03 14:35:51 +0200

    Add: boats-to-save-people - submission-3

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-3.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number[]} people
+     * @param {number} limit
+     * @return {number}
+     */
+    numRescueBoats(people, limit) {
+        people.sort((a,b) => b-a);
+
+        let i = 0;
+        let j = people.length-1;
+        let boats = 0;
+
+        while(i<=j) {
+            if(people[i]+people[j] <= limit) {
+                j--;
+            }
+
+            i++;
+            boats++;
+        }
+
+        return boats;
+    }
+}
