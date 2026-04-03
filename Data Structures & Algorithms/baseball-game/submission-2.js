5c2cc0d8c718365ac9d5db803ec6e251999a3e40
lucacasty <119264645+lucacasty@users.noreply.github.com>
2026-04-03 14:22:20 +0200

    Add: baseball-game - submission-2

---
+++ Data Structures & Algorithms/baseball-game/submission-2.js
@@ -0,0 +1,41 @@
+class Solution {
+    /**
+     * @param {string[]} operations
+     * @return {number}
+     */
+    calPoints(operations) {
+        const stack = [];
+
+        let res = 0;
+
+        for(let i=0;i<operations.length;i++) {
+            switch(operations[i]) {
+                case "+":
+                    if(stack.length >= 2) {
+                        const el = stack[stack.length-2]+stack[stack.length-1];
+                        stack.push(el);
+                        res += el;
+                    }
+                    break;
+                case "C":
+                    const el = stack.pop();
+                    res -= el;
+                    break;
+                case "D":
+                    if(stack.length >= 1) {
+                        const el = 2*stack[stack.length-1];
+                        stack.push(el);
+                        res += el;
+                    }
+                    break;
+                default:
+                    stack.push(parseInt(operations[i]));
+                    res += parseInt(operations[i]);
+                    break;
+            }
+        }
+
+        return res;
+
+    }
+}
