61beb20f2205d891fe493ef220cb78f6f9b8ba23
lucacasty <119264645+lucacasty@users.noreply.github.com>
2026-04-07 18:22:36 +0200

    Add: k-closest-points-to-origin - submission-5

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-5.js
@@ -0,0 +1,10 @@
+class Solution {
+    /**
+     * @param {number[][]} points
+     * @param {number} k
+     * @return {number[][]}
+     */
+    kClosest(points, k) {
+        return points.sort((a,b) => (Math.sqrt(Math.pow(a[0],2) + Math.pow(a[1],2)))-(Math.sqrt(Math.pow(b[0],2) + Math.pow(b[1],2)))).slice(0,k);
+    }
+}
