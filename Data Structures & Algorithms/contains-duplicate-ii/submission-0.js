cbf39470dd4634e628e88fabdb028bbe4b9aefce
lucacasty <119264645+lucacasty@users.noreply.github.com>
2026-04-08 12:41:27 +0200

    Add: contains-duplicate-ii - submission-0

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-0.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {boolean}
+     */
+    containsNearbyDuplicate(nums, k) {
+        const map = new Map();
+
+        for(let i=0;i<nums.length;i++) {
+            if(map.has(nums[i])) {
+                if(i-map.get(nums[i]) <= k) {
+                    return true;
+                } else {
+                    map.set(nums[i],i);
+                }
+
+            } else {
+                map.set(nums[i],i);
+            }
+        }
+
+        return false;
+    }
+}
