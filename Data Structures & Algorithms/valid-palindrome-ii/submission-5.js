bc06641f2c3803697a284d4ee409bd4745e5c6fd
lucacasty <119264645+lucacasty@users.noreply.github.com>
2026-04-09 14:23:47 +0200

    Add: valid-palindrome-ii - submission-5

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-5.js
@@ -0,0 +1,34 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    validPalindrome(s,i=0,j=null) {
+        let change = false;
+        if(j==null) {
+            j = s.length-1;
+        }
+
+        while(i < j) {
+            if(s[i] == s[j]) {
+                i++;
+                j--;
+            } else {
+                if(change)  return false;
+
+                change = true;
+                if(s[i] == s[j-1] && s[i+1] == s[j]) {
+                    return this.validPalindrome(s,i+1,j) || this.validPalindrome(s,i,j-1);
+                }else if(s[i] == s[j-1]) {
+                    j--;
+                } else if(s[i+1] == s[j]) {
+                    i++;
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
