842635c54bbc8bc38bb97e765f38ca320f7d607d
lucacasty <119264645+lucacasty@users.noreply.github.com>
2026-04-12 19:24:05 +0200

    Add: minimum-path-sum - submission-2

---
+++ Data Structures & Algorithms/minimum-path-sum/submission-2.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {number[][]} grid
+     * @return {number}
+     */
+    minPathSum(grid) {
+        const ROWS = grid.length;
+        const COLS = grid[0].length;
+        let dp = Array.from({length: ROWS+1}, () => new Array(COLS+1).fill(0));
+        
+        for(let i=ROWS-1;i>=0;i--) {
+            for(let j=COLS-1;j>=0;j--) {
+                dp[i][j] = grid[i][j];
+                if(i+1 < ROWS && j+1 < COLS) {
+                    dp[i][j] += Math.min(dp[i+1][j],dp[i][j+1]);
+                } else if(i+1<ROWS) {
+                    dp[i][j] += dp[i+1][j];
+                } else if(j+1<COLS) {
+                    dp[i][j] += dp[i][j+1];
+                }
+            }
+        }
+
+        return dp[0][0];
+    }
+}
