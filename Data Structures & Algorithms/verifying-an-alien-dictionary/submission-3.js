3b6e22675ab496fc8f792e7614d56856a300957a
lucacasty <119264645+lucacasty@users.noreply.github.com>
2026-04-18 16:15:16 +0200

    Add: verifying-an-alien-dictionary - submission-3

---
+++ Data Structures & Algorithms/verifying-an-alien-dictionary/submission-3.js
@@ -0,0 +1,33 @@
+class Solution {
+    /**
+     * @param {string[]} words
+     * @param {string} order
+     * @return {boolean}
+     */
+    isAlienSorted(words, order) {
+        if(words.length == 1) return true;
+
+        const charPosition = new Map();
+        for(let i=0;i<order.length;i++) {
+            charPosition.set(order[i],i);
+        }
+
+        for(let i=1;i<words.length;i++) {
+            let j=0;
+            while(j<Math.max(words[i-1].length,words[i].length)) {
+
+                if(words[i-1][j] !== words[i][j]) {
+                    if((charPosition.get(words[i-1][j]) || -1) > (charPosition.get(words[i][j]) || -1)) {
+                        return false;
+                    } else {
+                        break;
+                    }
+                }
+
+                j++;
+            }
+        }
+
+        return true;
+    }
+}
