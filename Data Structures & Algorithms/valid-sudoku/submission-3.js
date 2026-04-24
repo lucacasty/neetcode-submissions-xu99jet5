46c0e0353026b019747d69b9930a887cbc362b55
lucacasty <119264645+lucacasty@users.noreply.github.com>
2026-04-24 12:51:21 +0200

    Add: valid-sudoku - submission-3

---
+++ Data Structures & Algorithms/valid-sudoku/submission-3.js
@@ -0,0 +1,44 @@
+class Solution {
+    /**
+     * @param {character[][]} board
+     * @return {boolean}
+     */
+    isValidSudoku(board) {
+
+        const ROWS = board.length;
+        const COLS = board[0].length;
+
+        for(let i=0;i<ROWS;i++) {
+            let set = new Set();
+            for(let j=0;j<COLS;j++) {
+                if(board[i][j] == ".") continue;
+                if(set.has(board[i][j])) return false;
+                set.add(board[i][j]);
+            }
+        }
+
+        for(let i=0;i<COLS;i++) {
+            let set = new Set();
+            for(let j=0;j<ROWS;j++) {
+                if(board[j][i] == ".") continue;
+                if(set.has(board[j][i])) return false;
+                set.add(board[j][i]);
+            }
+        }
+
+        for(let startRow=0;startRow<ROWS;startRow+=3) {
+            for(let startCol=0;startCol<COLS;startCol+=3) {
+                let set = new Set();
+                for(let i=startRow;i<startRow+3;i++) {
+                    for(let j=startCol;j<startCol+3;j++) {
+                        if(board[i][j] == ".") continue;
+                        if(set.has(board[i][j])) return false;
+                        set.add(board[i][j]);
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+}
