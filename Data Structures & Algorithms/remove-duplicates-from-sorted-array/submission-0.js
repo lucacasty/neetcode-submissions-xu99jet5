dca545266aabb258448f013e70b06e98ee6a3039
lucacasty <119264645+lucacasty@users.noreply.github.com>
2026-04-27 19:07:34 +0200

    Add: remove-duplicates-from-sorted-array - submission-0

---
+++ Data Structures & Algorithms/remove-duplicates-from-sorted-array/submission-0.js
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    removeDuplicates(nums) {
+        let newIndex = 0;
+        for(let i=0;i<nums.length;i++) {
+            if(nums[i] !== nums[i+1]) {
+                nums[newIndex] = nums[i];
+                newIndex++;
+            }
+        }
+
+        return newIndex;
+    }
+}
