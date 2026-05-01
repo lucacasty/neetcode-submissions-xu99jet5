9021a628c15a1fd1ffa3073e75ee62a9ca3e801c
lucacasty <119264645+lucacasty@users.noreply.github.com>
2026-05-01 14:34:33 +0200

    Add: majority-element - submission-5

---
+++ Data Structures & Algorithms/majority-element/submission-5.js
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    majorityElement(nums) {
+        const map = new Map();
+        const midLength = nums.length/2;
+
+        for(let num of nums) {
+            let count = (map.get(num) || 0) + 1;
+            if(count > midLength)  return num;
+
+            map.set(num,count);
+        }
+    }
+}
