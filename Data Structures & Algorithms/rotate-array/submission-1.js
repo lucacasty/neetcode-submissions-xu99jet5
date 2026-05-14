e42f6f2a36777299062e0eb9688729d6514030ba
lucacasty <119264645+lucacasty@users.noreply.github.com>
2026-05-14 18:25:08 +0200

    Add: rotate-array - submission-1

---
+++ Data Structures & Algorithms/rotate-array/submission-1.js
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {void} Do not return anything, modify nums in-place instead.
+     */
+    rotate(nums, k) {
+        for(let i=0;i<k;i++) {
+            let lastEl = nums[nums.length-1];
+            for(let j=nums.length-1;j>0;j--) {
+                nums[j] = nums[j-1];
+            }
+            nums[0] = lastEl;
+        }
+    }
+}
