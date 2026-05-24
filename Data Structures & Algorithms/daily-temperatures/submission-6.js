efda1e11cd0f7649c0a301b510caa80cc50a6008
lucacasty <119264645+lucacasty@users.noreply.github.com>
2026-05-24 13:57:33 +0200

    Add: daily-temperatures - submission-6

---
+++ Data Structures & Algorithms/daily-temperatures/submission-6.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} temperatures
+     * @return {number[]}
+     */
+    dailyTemperatures(temperatures) {
+        let result = new Array(temperatures.length).fill(0);
+        let stack = [];
+
+        for(let i=0;i<temperatures.length;i++) {
+            while(stack.length > 0 && temperatures[stack[stack.length-1]] < temperatures[i]) {
+                let el = stack.pop();
+                result[el] = i-el;
+            }
+            stack.push(i);
+        }
+
+        return result;
+    }
+}
