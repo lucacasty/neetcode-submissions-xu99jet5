e3497cd364c0fe6a363394bec63f2dbf3c739195
lucacasty <119264645+lucacasty@users.noreply.github.com>
2026-06-08 12:46:48 +0200

    Add: search-insert-position - submission-7

---
+++ Data Structures & Algorithms/search-insert-position/submission-7.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    searchInsert(nums, target) {
+        let left = 0;
+        let right = nums.length-1;
+
+        while(left < right) {
+            let mid = Math.floor((right-left)/2) + left;
+
+            console.log(mid);
+
+            if(target > nums[mid]) {
+                left = mid+1;
+            } else if(target < nums[mid]) {
+                right = mid-1;
+            } else {
+                return mid;
+            }
+        }
+
+        return nums[left] < target ? left+1 : left;
+    }
+}
