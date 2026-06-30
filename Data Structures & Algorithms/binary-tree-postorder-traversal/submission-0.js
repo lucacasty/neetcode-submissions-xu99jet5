5c5e4a32468367b9c8962eb81303e7bef5ce1d45
lucacasty <119264645+lucacasty@users.noreply.github.com>
2026-06-30 19:17:47 +0200

    Add: binary-tree-postorder-traversal - submission-0

---
+++ Data Structures & Algorithms/binary-tree-postorder-traversal/submission-0.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {number[]}
+     */
+    postorderTraversal(root) {
+        const arr = [];
+        function dfs(node) {
+            if(!node)   return;
+
+            dfs(node.left);
+            dfs(node.right);
+
+            arr.push(node.val);
+        }
+
+        dfs(root);
+
+        return arr;
+    }
+}
