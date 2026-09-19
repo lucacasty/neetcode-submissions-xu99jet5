90585eddfc89c0d377be28508bcc7d0bc2e86ef2
lucacasty <119264645+lucacasty@users.noreply.github.com>
2026-09-19 10:57:51 +0200

    Add: merge-sorted-array - submission-8

---
+++ Data Structures & Algorithms/merge-sorted-array/submission-8.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} nums1
+     * @param {number} m
+     * @param {number[]} nums2
+     * @param {number} n
+     * @return {void} Do not return anything, modify nums1 in-place instead.
+     */
+    merge(nums1, m, nums2, n) {
+        let nums1Pointer = m-1;
+        let nums2Pointer = n-1;
+
+        for(let i = m+n-1;i>=0;i--) {
+            if(nums1[nums1Pointer] > nums2[nums2Pointer] || nums2Pointer<0) {
+                nums1[i] = nums1[nums1Pointer];
+                nums1Pointer--;
+            } else {
+                nums1[i] = nums2[nums2Pointer];
+                nums2Pointer--;
+            }
+        }
+    }
+}
